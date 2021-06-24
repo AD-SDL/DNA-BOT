a0c0df0bc9e9709b5d096256e80c9858f7997f45
Ashka Shah <shah38@clswl054.cels.anl.gov>
2021-06-24 15:21:39 -0500

    update clips script to use 8 channel functionality and add thermocycler resume capability

--- dnabot/template_ot2_scripts/clip_template_noPCR_2.8.py
+++ dnabot/template_ot2_scripts/clip_template_noPCR_2.8.py
@@ -142,13 +142,12 @@ def run(protocol):
 
         ### Transfers
 
-        '''
         # transfer master mix into destination wells
         # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
         pipette.pick_up_tip()
         pipette.transfer(MASTER_MIX_VOLUME, master_mix, destination_wells, blow_out=True, blowout_location='destination well', new_tip='never')
-        '''
 
+        '''
         # Implementing the transfer above with an 8-channel pipette.
         pipette.pick_up_tip(reverse_tips[tip_at // 96][tip_at % 96])
         for well in destination_wells:
@@ -159,10 +158,24 @@ def run(protocol):
         pipette.drop_tip()
 
         '''
-        # transfer water into destination wells
-        pipette.transfer(water_vols, water, destination_wells, blow_out=True, blowout_location='destination well', new_tip='always')
-        '''
 
+        # transfer water into destination wells
+        # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
+        # assume that each column has same volume
+        columns = len(water_vols) // 8 + 1
+        pipette.transfer(water_vols[0::8],
+                         water,
+                         destination_wells[0::8], blow_out=True, blowout_location='destination well', new_tip='always')
+
+        # We now need to switch the reverse pick algorithm so set an offset for the current rack
+        offset_by_rack = len(reverse_tips) * [0]
+        tip_at += columns * 8
+        current_rack = tip_at // 96
+        for i in range(len(offset_by_rack)):
+            if current_rack == i:
+                offset_by_rack[i] = tip_at
+
+        '''
         # implement the transfer above with an 8-channel pipette
         for i in range(len(destination_wells)):
             pipette.pick_up_tip(reverse_tips[tip_at // 96][tip_at % 96])
@@ -172,14 +185,20 @@ def run(protocol):
             tip_at += 1
 
         '''
-        #transfer prefixes, suffixes, and parts into destination wells
-            # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
-        # for clip_num in range(len(parts_wells)):
-        #     pipette.transfer(1, source_plates[prefixes_plates[clip_num]].wells(prefixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
-        #     pipette.transfer(1, source_plates[suffixes_plates[clip_num]].wells(suffixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
-        #     pipette.transfer(parts_vols[clip_num], source_plates[parts_plates[clip_num]].wells(parts_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=PART_MIX_SETTINGS)
 
         '''
+        #transfer prefixes, suffixes, and parts into destination wells
+            # added blowout into destination wells ('blowout_location' only works for API 2.8 and above)
+        for clip_num in range(len(parts_wells)):
+            pipette.transfer(1, source_plates[prefixes_plates[clip_num]].wells(prefixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
+            pipette.transfer(1, source_plates[suffixes_plates[clip_num]].wells(suffixes_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=LINKER_MIX_SETTINGS)
+            pipette.transfer(parts_vols[clip_num], source_plates[parts_plates[clip_num]].wells(parts_wells[clip_num]), destination_wells[clip_num], blow_out=True, blowout_location='destination well', new_tip='always', mix_after=PART_MIX_SETTINGS)
+        '''
+
+        # Calculates which rack and tip within rack to pick up based on how many have already been picked up
+        # accomodates a switch from 8 channel functionality with 'transfer' and 1 channel functionality
+        def get_tip(index, offsets, tips):
+            return tips[int(index // 96)][index % 96 - offsets[index // 96]]
 
         # Implementing the transfer above with an 8-channel pipette.
         def custom_transfer(vol, source, dest):
@@ -190,19 +209,19 @@ def run(protocol):
             pipette.drop_tip()
 
         for clip_num in range(len(parts_wells)):
-            pipette.pick_up_tip(reverse_tips[int(tip_at // 96)][tip_at % 96])
+            pipette.pick_up_tip(get_tip(tip_at, offset_by_rack, reverse_tips))
             custom_transfer(1,
                             source_plates[prefixes_plates[clip_num]].wells_by_name()[prefixes_wells[clip_num]],
                             destination_wells[clip_num])
             tip_at += 1
 
-            pipette.pick_up_tip(reverse_tips[int(tip_at // 96)][tip_at % 96])
-            custom_transfer( 1,
+            pipette.pick_up_tip(get_tip(tip_at, offset_by_rack, reverse_tips))
+            custom_transfer(1,
                             source_plates[suffixes_plates[clip_num]].wells_by_name()[suffixes_wells[clip_num]],
                             destination_wells[clip_num])
             tip_at += 1
 
-            pipette.pick_up_tip(reverse_tips[int(tip_at // 96)][tip_at % 96])
+            pipette.pick_up_tip(get_tip(tip_at, offset_by_rack, reverse_tips))
             custom_transfer(parts_vols[clip_num],
                             source_plates[parts_plates[clip_num]].wells_by_name()[parts_wells[clip_num]],
                             destination_wells[clip_num])
