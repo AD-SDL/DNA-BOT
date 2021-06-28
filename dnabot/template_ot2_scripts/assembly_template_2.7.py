9358c0b5ed5f37bbc999b0e11c29f01df3affacb
Ashka Shah <shah38@clswl053.cels.anl.gov>
2021-06-28 10:48:56 -0500

    add 8 channel distribute to assembly

--- dnabot/template_ot2_scripts/assembly_template_2.7.py
+++ dnabot/template_ot2_scripts/assembly_template_2.7.py
@@ -50,7 +50,6 @@ def run(protocol:protocol_api.ProtocolContext):
 
 
                 pipette = protocol.load_instrument('p20_multi_gen2', PIPETTE_MOUNT, tip_racks=tipracks)#old code: pipette = instruments.P10_Single(mount=PIPETTE_MOUNT, tip_racks=tipracks)
-                pipette.drop_tip()
                 # Define Labware and set temperature
                 magbead_plate = protocol.load_labware(MAG_PLATE_TYPE, MAG_PLATE_POSITION)
                #old code: magbead_plate = labware.load(MAG_PLATE_TYPE, MAG_PLATE_POSITION)
@@ -63,26 +62,47 @@ def run(protocol:protocol_api.ProtocolContext):
                 tempdeck.set_temperature(TEMP)
                #old code: destination_plate = labware.load(DESTINATION_PLATE_TYPE, TEMPDECK_SLOT, share=True)tempdeck.set_temperature(TEMP)tempdeck.wait_for_temp()
 
-                 # Master mix transfers
+                # Master mix transfers
                 final_assembly_lens = []
                 for values in final_assembly_dict.values():
                     final_assembly_lens.append(len(values))
                 unique_assemblies_lens = list(set(final_assembly_lens))
-                master_mix_well_letters = ['A', 'B', 'C', 'D']
-#                 master_mix_well_letters = ['A1', 'A2', 'A3', 'A4']
+                # master_mix_well_letters = ['A', 'B', 'C', 'D']
                 for x in unique_assemblies_lens:
-                    master_mix_well = master_mix_well_letters[(x - 1) // 6] + str(x - 1)
-                    destination_inds = [i for i, lens in enumerate(final_assembly_lens) if lens == x]
+                    # to use as 8 channel, start well must be A
+                    master_mix_well = 'A1'  # master_mix_well_letters[(x - 1) // 6] + str(x - 1)
+                    # assume that every 8th index starts at A so we can use 8 channel
+                    destination_inds = [i for i, lens in enumerate(final_assembly_lens) if lens == x][::8]
                     destination_wells = np.array([key for key, value in list(final_assembly_dict.items())])
                     destination_wells = list(destination_wells[destination_inds])
+                    destination_wells = [destination_plate.wells_by_name()[i] for i in destination_wells]
+                    pipette.distribute(TOTAL_VOL - x * PART_VOL, tube_rack.wells_by_name()[master_mix_well],
+                                     destination_wells, new_tip='once')  # transfer water and buffer in the pipette
+                    columns = len(destination_wells) // 8
+                    tip_at += 8 * columns  # 8 tips per column * number of columns
+
+                    '''
+                     1 channel code
                     pipette.pick_up_tip(reverse_tips[tip_at // 96][tip_at % 96])
                     for destination_well in destination_wells:# make tube_rack_wells and destination_plate.wells in the same type
-                        # pipette.transfer(TOTAL_VOL - x * PART_VOL, tube_rack.wells(master_mix_well),
-                        #                  destination_plate.wells(destination_well), new_tip='never')#transfer water and buffer in the pipette
                         pipette.aspirate(TOTAL_VOL - x * PART_VOL, tube_rack.wells_by_name()[master_mix_well])
                         pipette.dispense(TOTAL_VOL - x * PART_VOL,destination_plate.wells_by_name()[destination_well])
                     pipette.drop_tip()
-                    tip_at += 1
+                    tip_at +=1
+                    '''
+
+
+                # We now need to switch the reverse pick algorithm so set an offset for the current rack
+                offset_by_rack = len(reverse_tips) * [0]
+                current_rack = tip_at // 96
+                for i in range(len(offset_by_rack)):
+                    if current_rack == i:
+                        offset_by_rack[i] = tip_at
+
+                # Calculates which rack and tip within rack to pick up based on how many have already been picked up
+                # accomodates a switch from 8 channel functionality with 'transfer' and 1 channel functionality
+                def get_tip(index, offsets, tips):
+                    return tips[int(index // 96)][index % 96 - offsets[index // 96]]
 
                 # Part transfers
                 for key, values in list(final_assembly_dict.items()):
@@ -91,7 +111,7 @@ def run(protocol:protocol_api.ProtocolContext):
                         #                  destination_plate.wells(key), mix_after=MIX_SETTINGS,
                         #                  new_tip='always')#transfer parts in one tube
 
-                        pipette.pick_up_tip(reverse_tips[tip_at // 96][tip_at % 96])
+                        pipette.pick_up_tip(get_tip(tip_at, offset_by_rack, reverse_tips))
                         pipette.aspirate(PART_VOL, magbead_plate.wells_by_name()[value])
                         pipette.dispense(PART_VOL,destination_plate.wells_by_name()[key])
                         pipette.mix(3)
