246af44ae77ecce122ccd3e60b293d1a8280461a
Ashka Shah <shah38@Ashkas-MacBook-Pro.local>
2021-07-19 10:23:59 -0500

    add calibration protocols for upfront calibration

---
+++ dnabot/template_ot2_scripts/bio/calibration/calibration-part1.py
@@ -0,0 +1,32 @@
+# Final version of the purification template to be used in actual DNA assembly experiment
+# with all the correct labware
+
+metadata = {
+    'apiLevel': '2.8',
+    'protocolName': 'calibrate all labware for DNA Assembly protocols',
+    'author': 'shahashka@anl.gov',
+    'description': ''}
+
+
+def run(protocol):
+    tipracks_300 = [protocol.load_labware("opentrons_96_tiprack_300ul", 3)]
+
+    pipette_300 = protocol.load_instrument("p300_multi_gen2", mount="left", tip_racks=tipracks_300)
+
+    trough = protocol.load_labware("4Ti_0131_Reservoir_12_trough_21000ul", 6)
+    deep_well = protocol.load_labware("nest_96_wellplate_2ml_deep", 2)
+
+    TEMPDECK = protocol.load_module('temperature module gen2', 1)
+    THERMOCYCLER = protocol.load_module('Thermocycler Module') # default 10/7/11/8 slots
+    temp_plate = TEMPDECK.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
+    tc_plate = THERMOCYCLER.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
+
+    pipette_300.pick_up_tip()
+    pipette_300.aspirate(1, deep_well.wells_by_name()['A1'])
+    pipette_300.dispense(1, tc_plate.wells_by_name()['A1'])
+    pipette_300.return_tip()
+
+    pipette_300.pick_up_tip()
+    pipette_300.aspirate(1, trough.wells_by_name()['A1'])
+    pipette_300.dispense(1, temp_plate.wells_by_name()['A1'])
+    pipette_300.return_tip()
---
+++ dnabot/template_ot2_scripts/bio/calibration/calibration-part2.py
@@ -0,0 +1,30 @@
+# Final version of the purification template to be used in actual DNA assembly experiment
+# with all the correct labware
+
+metadata = {
+    'apiLevel': '2.8',
+    'protocolName': 'calibrate all labware for DNA Assembly protocols',
+    'author': 'shahashka@anl.gov',
+    'description': ''}
+
+
+def run(protocol):
+    tipracks_20 = [protocol.load_labware("opentrons_96_tiprack_20ul", 3)]
+
+    pipette_20 = protocol.load_instrument("p20_multi_gen2", mount="right", tip_racks=tipracks_20)
+
+    well_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", 6)
+    tube_rack = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", 2)
+
+    MAGDECK = protocol.load_module('magnetic module gen2', 1)
+    mag_plate = MAGDECK.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
+
+    pipette_20.pick_up_tip()
+    pipette_20.aspirate(1, tube_rack.wells_by_name()['A1'])
+    pipette_20.dispense(1, well_plate.wells_by_name()['A1'])
+    pipette_20.return_tip()
+
+    pipette_20.pick_up_tip()
+    pipette_20.aspirate(1, tube_rack.wells_by_name()['A1'])
+    pipette_20.dispense(1, mag_plate.wells_by_name()['A1'])
+    pipette_20.return_tip()
