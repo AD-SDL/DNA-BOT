fb789e3982b81a610c28b5ec70ed370c295526e6
Ashka Shah <shah38@Ashkas-MacBook-Pro.local>
2021-08-06 13:11:19 -0500

    short thermocycler script for testing

--- dnabot/dummy_scripts/thermocycler.py
+++ dnabot/dummy_scripts/thermocycler.py
@@ -9,10 +9,13 @@ metadata = {
 def run(protocol):
     # Thermocycler Module
     tc_mod = protocol.load_module('thermocycler module')
+    tc_mod.close_lid()
     tc_mod.set_lid_temperature(105)
-    tc_mod.set_block_temperature(50, hold_time_minutes=45, block_max_volume=15)
-    tc_mod.set_block_temperature(4, hold_time_minutes=2, block_max_volume=30)
-
+    # tc_mod.set_block_temperature(50, hold_time_minutes=45, block_max_volume=15)
+    # tc_mod.set_block_temperature(4, hold_time_minutes=2, block_max_volume=30)
+    #DEBUG
+    tc_mod.set_block_temperature(50, hold_time_minutes=1, block_max_volume=15)
+    tc_mod.set_block_temperature(4, hold_time_minutes=1, block_max_volume=30)
     # Increase the hold time at 4 C if necessary
     tc_mod.set_lid_temperature(37)
     tc_mod.open_lid()
